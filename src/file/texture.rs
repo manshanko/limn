448b094acd6b30b939def4b7ddfbcd77224e4d0d
ManShanko <deathage@gmail.com>
2023-09-07 15:15:14 -0700

    Improve readability of texture extract code

--- src/file/texture.rs
+++ src/file/texture.rs
@@ -1,6 +1,17 @@
+//! Extractor for Darktide `texture` files.
+//!
+//! The `texture` format in Darktide is a small wrapper around a DDS texture.
+//! DDS textures with mipmaps will stored larger mipmaps at a `data/*/*` path.
+//! Larger mipmaps are chunked to maintain width of smallest mipmap.
+//!
+//! Only the largest mipmap in a `texture` is extracted. When extracting the
+//! mipmap chunks are sorted to restore image dimensions.
+
 use std::fs::File;
 use crate::read::ChunkReader;
 use super::*;
+
+const DDSD_MIPMAPCOUNT: u32 = 0x20000;
 
 pub(crate) struct TextureParser;
 
@@ -10,7 +21,7 @@ impl Extractor for TextureParser {
         entry: &mut Entry<'_, '_>,
         file_path: &Path,
         mut shared: &mut [u8],
-        shared_flex: &mut Vec<u8>,
+        memory_pool: &mut Vec<u8>,
         options: &ExtractOptions<'_>,
     ) -> io::Result<u64> {
         let variants = entry.variants();
@@ -19,25 +30,14 @@ impl Extractor for TextureParser {
 
         let has_high_res = prime.unknown1 == 0 && prime.tail_size > 0;
         let unknown1 = prime.unknown1;
-        let mut rdr = match unknown1 {
+        let mut either_rdr = match unknown1 {
             0 => Ok(entry),
             1 => {
-                assert!(prime.tail_size == 0);
+                assert_eq!(0, prime.tail_size);
 
-                let mut buffer = [0_u8; 31];
-                entry.read(&mut buffer).unwrap();
-                let data_path = data_path_from(&buffer).unwrap();
-                let path = path_concat(options.target, &mut shared, data_path, None);
-                assert!(path.starts_with(options.target));
-                let Ok(file) = File::open(path) else {
-                    if cfg!(debug_assertions) {
-                        panic!("failed to load resource file at {data_path}");
-                    } else {
-                        eprintln!("failed to load resource file at {data_path}");
-                    }
-                    return Err(io::Error::new(io::ErrorKind::NotFound,
-                        "failed to find texture resource file under data/**/*"));
-                };
+                let mut data_path = [0_u8; 31];
+                entry.read(&mut data_path).unwrap();
+                let file = data_path_from_buffer(shared, options.target, &data_path).unwrap();
                 let slice;
                 (slice, shared) = shared.split_at_mut(0x10000);
                 Err(ChunkReader::new(slice, file))
@@ -45,7 +45,7 @@ impl Extractor for TextureParser {
             unk => panic!("unexpected Entry.unknown1 {unk}"),
         };
 
-        let rdr: &mut dyn Read = match &mut rdr {
+        let rdr: &mut dyn Read = match &mut either_rdr {
             Ok(f) => f,
             Err(f) => f,
         };
@@ -58,7 +58,7 @@ impl Extractor for TextureParser {
             let inflate_size = rdr.read_u32::<LE>().unwrap() as usize;
             assert!(inflate_size >= 148, "{inflate_size}");
 
-            let ([in_buf, out_buf, scratch], _) = split_vec(shared_flex,
+            let ([in_buf, out_buf, scratch], _) = split_vec(memory_pool,
                 [deflate_size, inflate_size, 0x100000]);
             rdr.read_exact(in_buf).unwrap();
             let size = options.oodle.decompress(in_buf, out_buf, scratch).unwrap();
@@ -92,14 +92,7 @@ impl Extractor for TextureParser {
                 assert!(has_high_res);
                 assert_eq!(0x44583130_u32.swap_bytes(), fourcc);
 
-                let mut dxt10 = &out_buf[128..148];
-                let _encoding_kind = dxt10.read_u32::<LE>().unwrap();
-                let dimension = dxt10.read_u32::<LE>().unwrap();
-                let _misc_flags = dxt10.read_u32::<LE>().unwrap();
-                let array_size = dxt10.read_u32::<LE>().unwrap();
-                let _misc_flags2 = dxt10.read_u32::<LE>().unwrap();
-                assert_eq!(3, dimension);
-                assert_eq!(1, array_size);
+                check_dxt10(&out_buf[128..148]);
 
                 let num_chunks = u16::try_from(rdr.read_u32::<LE>().unwrap()).unwrap();
                 assert_eq!(8 + num_chunks * 4, meta_size);
@@ -115,8 +108,8 @@ impl Extractor for TextureParser {
                 }
                 let _unknown = rdr.read_u32::<LE>().unwrap();
 
-                let mut stream = [0; 31];
-                rdr.read_exact(&mut stream).unwrap();
+                let mut data_path = [0; 31];
+                rdr.read_exact(&mut data_path).unwrap();
                 assert!(rdr.read_u8().is_err());
 
                 let base_width = u32::from_le_bytes(<[u8; 4]>::try_from(&out_buf[16..20]).unwrap());
@@ -126,8 +119,13 @@ impl Extractor for TextureParser {
                 let block_size = 4 * base_pitch / base_width;
 
                 let pitch = largest_width / 4 * block_size;
-                let flags = u32::from_le_bytes(<[u8; 4]>::try_from(&out_buf[8..12]).unwrap());
-                let flags = flags & !0x20000;
+                let mut flags = u32::from_le_bytes(<[u8; 4]>::try_from(&out_buf[8..12]).unwrap());
+
+                // disable flag DDSD_MIPMAPCOUNT for output
+                // since only the largest mipmap is extracted
+                flags &= !DDSD_MIPMAPCOUNT;
+
+                // patch DDS header to use with largest mipmap
                 out_buf[8..12].copy_from_slice(&flags.to_le_bytes());
                 out_buf[12..16].copy_from_slice(&largest_height.to_le_bytes());
                 out_buf[16..20].copy_from_slice(&largest_width.to_le_bytes());
@@ -152,54 +150,22 @@ impl Extractor for TextureParser {
                 let num_chunks = chunk_width * chunk_height;
                 assert!(chunks.len() >= num_chunks as usize);
 
-                let window_size = (pitch * 64) as usize;
-                let ([in_buf, out_buf, scratch, window], _) = split_vec(shared_flex,
-                    [0x11000, 0x10000, 0x100000, window_size]);
-
-                let data_fd = {
-                    let data_path = data_path_from(&stream).unwrap();
-                    let data_path = path_concat(options.target, &mut shared, data_path, None);
-                    assert!(data_path.starts_with(options.target));
-                    let Ok(data_fd) = File::open(data_path) else {
-                        if cfg!(debug_assertions) {
-                            panic!("failed to load resource file at {}", data_path.display());
-                        } else {
-                            eprintln!("failed to load resource file at {}", data_path.display());
-                        }
-                        return Err(io::Error::new(io::ErrorKind::NotFound,
-                            "failed to find texture resource file under data/**/*"));
-                    };
-                    data_fd
-                };
-
+                let data_fd = data_path_from_buffer(shared, options.target, &data_path).unwrap();
                 let slice;
                 (slice, _) = shared.split_at_mut(0x10000);
                 let mut data_rdr = ChunkReader::new(slice, data_fd);
-                for (i, &chunk) in chunks.iter().take(num_chunks as usize).enumerate() {
-                    let in_buf = &mut in_buf[..chunk as usize];
-                    data_rdr.read_exact(in_buf).unwrap();
-                    let size = options.oodle.decompress(in_buf, out_buf, scratch).unwrap();
-                    assert_eq!(size, out_buf.len() as u64);
-                    assert_eq!(size, 0x10000);
 
-                    let i = i as u32;
-                    if i > 0 && i % chunk_width == 0 {
-                        out_fd.write_all(&window).unwrap();
-                        wrote += window.len();
-                    }
-
-                    assert_eq!((pitch / chunk_width) as u64, size / 64);
-
-                    let row_size = (chunk_width_pixel * block_size) as usize;
-                    let chunk_x = (i % chunk_width) * chunk_width_pixel * block_size;
-                    for (row_i, row) in out_buf.chunks_exact(row_size).enumerate() {
-                        let row_i = row_i as u32;
-                        let start = chunk_x + row_i * pitch;
-                        window[start as usize..start as usize + row_size].copy_from_slice(row);
-                    }
-                }
-                out_fd.write_all(&window).unwrap();
-                wrote += window.len();
+                wrote += sort_write_texture_chunks(
+                    memory_pool,
+                    &options.oodle,
+                    &mut data_rdr,
+                    &chunks[..num_chunks as usize],
+                    chunk_width,
+                    chunk_width_pixel,
+                    block_size,
+                    pitch,
+                    &mut out_fd,
+                );
                 wrote as u64
             };
             Ok(wrote)
@@ -214,4 +180,93 @@ impl Extractor for TextureParser {
             unreachable!()
         }
     }
+}
+
+fn data_path_from_buffer(
+    mut shared: &mut [u8],
+    target: &Path,
+    path: &[u8; 31],
+) -> io::Result<File> {
+    let path = data_path_from(&*path).unwrap();
+    let path = path_concat(target, &mut shared, path, None);
+    assert!(path.starts_with(target));
+    let Ok(fd) = File::open(path) else {
+        if cfg!(debug_assertions) {
+            panic!("failed to load resource file at {}", path.display());
+        } else {
+            eprintln!("failed to load resource file at {}", path.display());
+        }
+        return Err(io::Error::new(io::ErrorKind::NotFound,
+            "failed to find texture resource file under data/*/*"));
+    };
+    Ok(fd)
+}
+
+fn check_dxt10(mut dxt10: &[u8]) {
+    let _encoding_kind = dxt10.read_u32::<LE>().unwrap();
+    let dimension = dxt10.read_u32::<LE>().unwrap();
+    let _misc_flags = dxt10.read_u32::<LE>().unwrap();
+    let array_size = dxt10.read_u32::<LE>().unwrap();
+    let _misc_flags2 = dxt10.read_u32::<LE>().unwrap();
+    assert_eq!(3, dimension);
+    assert_eq!(1, array_size);
+}
+
+// Write out DDS texture while sorting chunks to restore texture dimensions.
+//
+// For example chunks like
+// ```
+// 1
+// 2
+// 3
+// 4
+// ```
+// will be sorted into
+// ```
+// 12
+// 34
+// ```
+fn sort_write_texture_chunks(
+    memory_pool: &mut Vec<u8>,
+    oodle: &Oodle,
+    data_rdr: &mut ChunkReader<File>,
+    chunks: &[u32],
+    chunk_width: u32,
+    chunk_width_pixel: u32,
+    block_size: u32,
+    pitch: u32,
+    out_fd: &mut File,
+) -> u64 {
+    let window_size = (pitch * 64) as usize;
+    let ([in_buf, out_buf, scratch, window], _) = split_vec(memory_pool,
+        [0x11000, 0x10000, 0x100000, window_size]);
+
+    let mut wrote = 0;
+    for (i, &chunk) in chunks.iter().enumerate() {
+        let in_buf = &mut in_buf[..chunk as usize];
+        data_rdr.read_exact(in_buf).unwrap();
+        let size = oodle.decompress(in_buf, out_buf, scratch).unwrap();
+        assert_eq!(size, out_buf.len() as u64);
+        assert_eq!(size, 0x10000);
+
+        let i = i as u32;
+        if i > 0 && i % chunk_width == 0 {
+            out_fd.write_all(&window).unwrap();
+            wrote += window.len();
+        }
+
+        assert_eq!((pitch / chunk_width) as u64, size / 64);
+
+        let row_size = (chunk_width_pixel * block_size) as usize;
+        let chunk_x = (i % chunk_width) * chunk_width_pixel * block_size;
+        for (row_i, row) in out_buf.chunks_exact(row_size).enumerate() {
+            let row_i = row_i as u32;
+            let start = chunk_x + row_i * pitch;
+            window[start as usize..start as usize + row_size].copy_from_slice(row);
+        }
+    }
+    out_fd.write_all(&window).unwrap();
+    wrote += window.len();
+
+    wrote as u64
 }
