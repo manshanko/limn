78a56cf7a29970d524ca4053915a1eff6ccd58ca
ManShanko <deathage@gmail.com>
2023-11-17 18:14:05 -0800

    Change murmurhash64a implementation

--- src/hash.rs
+++ src/hash.rs
@@ -103,54 +103,93 @@ pub(crate) const fn murmurhash64(key: &[u8]) -> u64 {
     murmur_hash64a(key, 0)
 }
 
-#[allow(clippy::identity_op)]
-// Copyright (c) 2014-2016, Jan-Erik Rediger
-// https://github.com/badboy/murmurhash64-rs/blob/3f9a5821650de6ee12f3cc45701444171ce30ebf/LICENSE
-//
-// https://github.com/badboy/murmurhash64-rs/blob/3f9a5821650de6ee12f3cc45701444171ce30ebf/src/lib.rs#L44
-pub(crate) const fn murmur_hash64a(key: &[u8], seed: u64) -> u64 {
-    let m : u64 = 0xc6a4a7935bd1e995;
-    let r : u8 = 47;
+#[inline]
+pub const fn murmur_hash64a(mut key: &[u8], seed: u64) -> u64 {
+    const MAGIC: u64 = 0xc6a4a7935bd1e995;
+    const ROLL: u8 = 47;
 
-    let len = key.len();
-    let mut h : u64 = seed ^ ((len as u64).wrapping_mul(m));
+    let mut hash = seed ^ (key.len() as u64).wrapping_mul(MAGIC);
 
-    let endpos = len-(len&7);
-    let mut i = 0;
-    while i != endpos {
-        let mut k : u64;
+    while key.len() > 7 {
+        let chunk;
+        (chunk, key) = key.split_at(8);
 
-        k  = key[i+0] as u64;
-        k |= (key[i+1] as u64) << 8;
-        k |= (key[i+2] as u64) << 16;
-        k |= (key[i+3] as u64) << 24;
-        k |= (key[i+4] as u64) << 32;
-        k |= (key[i+5] as u64) << 40;
-        k |= (key[i+6] as u64) << 48;
-        k |= (key[i+7] as u64) << 56;
+        // <[u8; 8]>::try_from() not usable in const
+        let mut k = u64::from_le_bytes([
+            chunk[0],
+            chunk[1],
+            chunk[2],
+            chunk[3],
+            chunk[4],
+            chunk[5],
+            chunk[6],
+            chunk[7],
+        ]);
 
-        k = k.wrapping_mul(m);
-        k ^= k >> r;
-        k = k.wrapping_mul(m);
-        h ^= k;
-        h = h.wrapping_mul(m);
+        k = k.wrapping_mul(MAGIC);
+        k ^= k >> ROLL;
+        k = k.wrapping_mul(MAGIC);
+        hash ^= k;
+        hash = hash.wrapping_mul(MAGIC);
+    }
 
-        i += 8;
-    };
+    if !key.is_empty() {
+        let mut xor = u64::to_ne_bytes(0);
+        let rem = key.len();
+        if rem >= 4 {
+            xor[0] = key[0];
+            xor[1] = key[1];
+            xor[2] = key[2];
+            xor[3] = key[3];
+            if rem >= 6 {
+                xor[4] = key[4];
+                xor[5] = key[5];
+                if rem == 7 {
+                    xor[6] = key[6];
+                }
+            } else if rem == 5 {
+                xor[4] = key[4];
+            }
+        } else if rem >= 2 {
+            xor[0] = key[0];
+            xor[1] = key[1];
+            if rem == 3 {
+                xor[2] = key[2];
+            }
+        } else if rem == 1 {
+            xor[0] = key[0];
+        }
 
-    let over = len & 7;
-    if over == 7 { h ^= (key[i+6] as u64) << 48; }
-    if over >= 6 { h ^= (key[i+5] as u64) << 40; }
-    if over >= 5 { h ^= (key[i+4] as u64) << 32; }
-    if over >= 4 { h ^= (key[i+3] as u64) << 24; }
-    if over >= 3 { h ^= (key[i+2] as u64) << 16; }
-    if over >= 2 { h ^= (key[i+1] as u64) << 8; }
-    if over >= 1 { h ^= key[i+0] as u64; }
-    if over >  0 { h = h.wrapping_mul(m); }
+        hash ^= u64::from_le_bytes(xor);
+        hash = hash.wrapping_mul(MAGIC);
+    }
 
-    h ^= h >> r;
-    h = h.wrapping_mul(m);
-    h ^= h >> r;
-    h
+    hash ^= hash >> ROLL;
+    hash = hash.wrapping_mul(MAGIC);
+    hash ^= hash >> ROLL;
+    hash
 }
 
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn mmh64a() {
+        const CHECK: &[(&[u8], u64)] = &[
+            (b"", 0),
+            (b"t", 0xa9f7b29f271e2bf0),
+            (b"te", 0x09a5c91602af86bf),
+            (b"tes", 0xdd890a49d3dbcc17),
+            (b"test", 0x2f4a8724618f4c63),
+            (b"testh", 0x897d3d790c864055),
+            (b"testha", 0xbc03666f652e7504),
+            (b"testhas", 0xc9735c8662b71bf6),
+            (b"testhash", 0x78409ab9ed54c450),
+        ];
+
+        for (key, hash) in CHECK {
+            assert_eq!(*hash, murmur_hash64a(key, 0));
+        }
+    }
+}
